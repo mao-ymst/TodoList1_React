8c4c45eaf8621a4cf61603b8a783b9d1fe456834
mao-ymst <47tegu8yun@gmail.com>
2026-04-23 12:51:33 +0900

    feat: add task and split TodoInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,61 @@
-import TodoItem from "./component/TodoItem"
+import React, { useState } from 'react'
+import TodoInput from './component/TodoInput';
 
-//1 仮のtodoの内容を用意
-const MOCK_TODOS = [
-  { id: 1, title: "Reactの基礎を学ぶ", isCompleted: false },
-  { id: 2, title: "Todo Listを作る", isCompleted: false },
-  { id: 3, title: "手書きノートにまとめる", isCompleted: true },
-]
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [inputText, setInputText] = useState("");
 
-function App() {
+  const addTask = () => {
+    if(inputText === "" || !inputText.trim) return;
+
+    const newTask = {
+      id: Date.now(),
+      text: inputText,
+      isDone: false,
+      createdAt: new Date().toLocaleDateString()
+    };
+
+    setTasks([...tasks, newTask]);
+
+    setInputText("")
+  }
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  }
 
   return (
-    <>
-      <div>
-        <h1>Todo List</h1>
-        <ul>
-          {MOCK_TODOS.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} />
-          ))}
-
-        </ul>
-
-      </div>
-    </>
+    <div style={{padding: '20px'}}>
+      <h1>My Todo List</h1>
+      <TodoInput value={inputText} onChange={handleChange} onAdd={addTask}/>
+      <p>入力中の文字：{inputText}</p>
+      <ul>
+        {tasks.map((task) => {
+          return (
+          <li key={task.id}>
+            {task.text}
+            <span>({task.createdAt})</span>
+          </li>
+        )
+      })
+      }
+      </ul>
+    </div>
   )
 }
 
-export default App
 
-//2 仮のTodoの内容をlistとして画面に表示する
-//3 TodoItemを呼び出し、propsを渡す
+//1 まずは大きな枠で動くものを造ってから分割
+//入力テキストを記憶させる箱を作る
+//文字が入力したらstateが更新するようにする　onChange
+// inputとstateを連携させる
+
+//2 複数のtaskを保存するためにstateを作成
+//新しいtaskのオブジェクトを作成
+//配列を更新して　今の配列に新しいtaskを合体させる --> 入力欄空にする
+//task追加用にbuttonに関数をつける
+
+//3 tasksにたまったデータを取り出してlistで表示する map
+//list １つ１つにユニークなkeyをつける　判別できるように
+
+//4 TodoInput.jsxの呼び出しと必要なpropsを渡す
---
+++ src/component/TodoInput.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function TodoInput({ inputText, onChange, onAdd }) {
+    return (
+        <div>
+            <input
+                type='text'
+                placeholder='タスクを入力してください'
+                value={inputText}
+                onChange={onChange} />
+            <button onAdd={onAdd}>追加</button>
+
+        </div>
+    )
+}
+
+
+//4 入力欄とボタン担当のページを作る
+//TodoInput：子　が文字を受け取ったら　App：親　へ報告できるように　props用意
